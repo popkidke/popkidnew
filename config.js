3449921f4627fa0da02ecd1eab41bd9444b2fd8e
popkidke <popxmd@gmail.com>
2026-04-28 01:31:30 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || 'POPKID;;;YOUR_ID_HERE', 
+    PREFIX: '.',
+    OWNER_NAME: 'Your Name',
+    BOT_NAME: 'ULTRA-BOT V1',
+    AUTO_READ: true,
+    THUMBNAIL: 'https://telegra.ph/file/your-logo.jpg' // Link to your bot's profile image
+};
